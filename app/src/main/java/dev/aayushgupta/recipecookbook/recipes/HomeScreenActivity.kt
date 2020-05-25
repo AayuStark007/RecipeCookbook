15d97ffbbd3ee8de7ecbead51f50abfa201dd0d2
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-05-25 21:21:58 +0530

    setup basic activity frame

---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipes/HomeScreenActivity.kt
@@ -0,0 +1,49 @@
+package dev.aayushgupta.recipecookbook.recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import dev.aayushgupta.recipecookbook.R
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupNavigationDrawer()
+        setSupportActionBar(findViewById(R.id.home_toolbar))
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfig =
+            AppBarConfiguration.Builder()
+                .setDrawerLayout(drawerLayout)
+                .build()
+
+        setupActionBarWithNavController(navController, appBarConfig)
+        findViewById<NavigationView>(R.id.home_nav_view)
+            .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfig)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigationDrawer() {
+        drawerLayout = (findViewById<DrawerLayout>(R.id.home_drawer_layout))
+            .apply {
+                setStatusBarBackground(R.color.colorPrimaryDark)
+            }
+    }
+}
