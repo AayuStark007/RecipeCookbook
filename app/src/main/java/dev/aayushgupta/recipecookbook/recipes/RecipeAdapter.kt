3daf6d6e4905b49ebfbef1453df0cf95bd22cae4
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-05-25 23:49:24 +0530

    setup initial adapter and list view implementation

---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipes/RecipeAdapter.kt
@@ -0,0 +1,49 @@
+package dev.aayushgupta.recipecookbook.recipes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import dev.aayushgupta.recipecookbook.data.Recipe
+import dev.aayushgupta.recipecookbook.databinding.ListItemRecipeBinding
+
+class RecipeAdapter: ListAdapter<Recipe, RecipeAdapter.RecipeViewHolder>(RecipeDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        return RecipeViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    fun submitRecipeList(list: List<Recipe>?) {
+        submitList(list)
+    }
+
+    class RecipeViewHolder private constructor(private val binding: ListItemRecipeBinding): RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(parent: ViewGroup): RecipeViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemRecipeBinding.inflate(layoutInflater, parent, false)
+                return RecipeViewHolder(binding)
+            }
+        }
+
+        fun bind(item: Recipe) {
+
+        }
+    }
+}
+
+class RecipeDiffCallback: DiffUtil.ItemCallback<Recipe>() {
+    override fun areItemsTheSame(oldItem: Recipe, newItem: Recipe): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Recipe, newItem: Recipe): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/dev/aayushgupta/recipecookbook/recipes/RecipesFragment.kt
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipes/RecipesFragment.kt
@@ -1,19 +1,58 @@
 package dev.aayushgupta.recipecookbook.recipes
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.aayushgupta.recipecookbook.R
+import dev.aayushgupta.recipecookbook.data.Recipe
+import dev.aayushgupta.recipecookbook.databinding.FragmentRecipesBinding
 
 class RecipesFragment: Fragment() {
+
+    private lateinit var fragmentRecipesBinding: FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        fragmentRecipesBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes, container, false)
+
+        val adapter = RecipeAdapter()
+        adapter.submitRecipeList(listOf(
+            Recipe(),Recipe(),Recipe(),Recipe(),Recipe(),
+            Recipe(),Recipe(),Recipe(),Recipe(),Recipe(),
+            Recipe(),Recipe(),Recipe(),Recipe(),Recipe(),
+            Recipe(),Recipe(),Recipe(),Recipe(),Recipe(),
+            Recipe(),Recipe(),Recipe(),Recipe(),Recipe()))
+
+        val manager = GridLayoutManager(activity, 2)
+        fragmentRecipesBinding.recipeList.layoutManager = manager
+        fragmentRecipesBinding.recipeList.addItemDecoration(ItemSpacingDecoration(8, 2))
+        fragmentRecipesBinding.recipeList.adapter = adapter
+
+        return fragmentRecipesBinding.root
+    }
+}
+
+class ItemSpacingDecoration(private var space: Int, private var span: Int): RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.left = space
+        outRect.top = space*2
+
+        if ((parent.getChildLayoutPosition(view)+1) % span == 0) {
+            outRect.right = space
+        }
     }
 }
