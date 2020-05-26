c1441d27032dc1dbc583d5c194b7d252977f59cf
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-05-27 01:00:00 +0530

    showing sample images for now

--- app/src/main/java/dev/aayushgupta/recipecookbook/recipes/BindingUtils.kt
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipes/BindingUtils.kt
@@ -1,8 +1,19 @@
 package dev.aayushgupta.recipecookbook.recipes
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import dev.aayushgupta.recipecookbook.data.domain.Recipe
+
+@BindingAdapter("recipeImage")
+fun ImageView.setRecipeImage(recipe: Recipe?) {
+    recipe?.let {
+        Glide.with(context)
+            .load(recipe.images[0].uri)
+            .into(this)
+    }
+}
 
 @BindingAdapter("recipeTitleText")
 fun TextView.setRecipeTitleText(recipe: Recipe?) {
--- app/src/main/java/dev/aayushgupta/recipecookbook/utils/DataHelpers.kt
+++ app/src/main/java/dev/aayushgupta/recipecookbook/utils/DataHelpers.kt
@@ -5,79 +5,80 @@ import dev.aayushgupta.recipecookbook.data.domain.Recipe
 import dev.aayushgupta.recipecookbook.data.domain.RecipeImage
 import dev.aayushgupta.recipecookbook.data.domain.RecipeTime
 import dev.aayushgupta.recipecookbook.data.domain.TimeUnit
+import java.util.*
 
-fun provideSampleRecipes(width: Int = 200, height: Int = 300): List<Recipe> {
+fun provideSampleRecipes(width: Int = 200, height: Int = 200): List<Recipe> {
     return listOf(
         Recipe(title = "Chicken Tikka", cuisine = "Indian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Paneer Tikka", cuisine = "Indian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Gobhi Tikka", cuisine = "Indian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Pasta", cuisine = "Italian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Samovar", cuisine = "Persian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Yakhni Pulao", cuisine = "Iranian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Vanilla Ice Cream", cuisine = "Contemporary",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Bhindi", cuisine = "Indian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Puliogare", cuisine = "South Indian",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Noodles", cuisine = "Chinese",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Fried Rice", cuisine = "Chinese",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             )),
 
         Recipe(title = "Ilish Macch", cuisine = "Bengali",
             cookingTime = RecipeTime(value = 45F, unit = TimeUnit.MINUTES),
             images = listOf(
-                RecipeImage(uri = Uri.parse("https://picsum.photos/$width/$height"), isLocal = false)
+                RecipeImage(uri = Uri.parse("https://picsum.photos/seed/${UUID.randomUUID()}/$width/$height"), isLocal = false)
             ))
 
     )
