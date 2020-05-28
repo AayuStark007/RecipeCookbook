188d01118db77bf483cce69eeb584e33a2b1fa33
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-05-29 01:24:38 +0530

    add view extensions for snackbar

---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/utils/ViewExtensions.kt
@@ -0,0 +1,25 @@
+package dev.aayushgupta.recipecookbook.utils
+
+import android.view.View
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+
+fun View.setupSnackbar(
+    lifecycleOwner: LifecycleOwner,
+    snackbarEvent: LiveData<Event<Int>>,
+    timeLength: Int
+) {
+    snackbarEvent.observe(lifecycleOwner, Observer { event ->
+        event.getContentIfNotHandled()?.let {
+            showSnackbar(context.getString(it), timeLength)
+        }
+    })
+}
+
+fun View.showSnackbar(snackbarText: String, timeLength: Int) {
+    Snackbar.make(this, snackbarText, timeLength).run {
+        show()
+    }
+}
