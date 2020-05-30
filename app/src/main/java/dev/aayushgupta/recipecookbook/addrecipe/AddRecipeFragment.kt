854cd7a3a76f37b67243b81f48dde4d6c18b0299
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-05-30 13:46:21 +0530

    create add recipe fragment and add some layout

---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/addrecipe/AddRecipeFragment.kt
@@ -0,0 +1,18 @@
+package dev.aayushgupta.recipecookbook.addrecipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AddRecipeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/addrecipe/AddRecipeViewModel.kt
@@ -0,0 +1,4 @@
+package dev.aayushgupta.recipecookbook.addrecipe
+
+class AddRecipeViewModel {
+}
