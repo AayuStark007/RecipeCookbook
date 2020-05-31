e16f2f4878c5fd1e47220062de6eeade27062c91
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-06-01 01:00:00 +0530

    completed basic detail flow

--- app/src/main/java/dev/aayushgupta/recipecookbook/recipedetails/RecipeDetailFragment.kt
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipedetails/RecipeDetailFragment.kt
@@ -1,7 +1,85 @@
 package dev.aayushgupta.recipecookbook.recipedetails
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import dev.aayushgupta.recipecookbook.R
+import dev.aayushgupta.recipecookbook.data.repository.DefaultRecipeRepository
+import dev.aayushgupta.recipecookbook.databinding.FragmentRecipedetailBinding
+import dev.aayushgupta.recipecookbook.utils.EventObserver
+import dev.aayushgupta.recipecookbook.utils.setupSnackbar
+import timber.log.Timber
 
 class RecipeDetailFragment : Fragment() {
-    
+
+    private lateinit var fragmentRecipedetailBinding: FragmentRecipedetailBinding
+
+    private val args: RecipeDetailFragmentArgs by navArgs()
+
+    private val viewModel by viewModels<RecipeDetailsViewModel> {
+        RecipeDetailsViewModelFactory(DefaultRecipeRepository.getRepository(requireActivity().application))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_recipedetail, container, false)
+        fragmentRecipedetailBinding = FragmentRecipedetailBinding.bind(view).apply {
+            viewmodel = viewModel
+        }
+
+        fragmentRecipedetailBinding.lifecycleOwner = this.viewLifecycleOwner
+
+        setupViewModel()
+
+
+        setHasOptionsMenu(true)
+        return fragmentRecipedetailBinding.root
+    }
+
+    private fun setupViewModel() {
+        viewModel.start(args.recipeId)
+
+        viewModel.recipe.observe(viewLifecycleOwner, Observer {
+            fragmentRecipedetailBinding.recipe = it
+        })
+
+        viewModel.isDataAvailable.observe(viewLifecycleOwner, Observer {
+            Timber.d("Data Available: $it")
+        })
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupSnackbar()
+        setupNavigation()
+    }
+
+    private fun setupSnackbar() {
+        view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
+    }
+
+    private fun setupNavigation() {
+        viewModel.deleteRecipeEvent.observe(viewLifecycleOwner, EventObserver {
+            // Goto home
+        })
+
+        viewModel.editRecipeEvent.observe(viewLifecycleOwner, EventObserver {
+            val action = RecipeDetailFragmentDirections
+                .actionRecipeDetailFragmentToAddRecipeFragment(
+                    args.recipeId,
+                    resources.getString(R.string.edit_recipe)
+                )
+            findNavController().navigate(action)
+        })
+    }
 }
--- app/src/main/java/dev/aayushgupta/recipecookbook/recipedetails/RecipeDetailsViewModel.kt
+++ app/src/main/java/dev/aayushgupta/recipecookbook/recipedetails/RecipeDetailsViewModel.kt
@@ -1,15 +1,77 @@
 package dev.aayushgupta.recipecookbook.recipedetails
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.annotation.StringRes
+import androidx.lifecycle.*
+import dev.aayushgupta.recipecookbook.R
 import dev.aayushgupta.recipecookbook.data.IRecipeRepository
+import dev.aayushgupta.recipecookbook.data.domain.Recipe
+import dev.aayushgupta.recipecookbook.utils.Event
+import dev.aayushgupta.recipecookbook.utils.Result
+import kotlinx.coroutines.launch
 
 class RecipeDetailsViewModel(private val recipeRepository: IRecipeRepository): ViewModel() {
+    private val _recipeId = MutableLiveData<String>()
 
+    private val _recipe = _recipeId.switchMap { recipeId ->
+        recipeRepository.observeRecipe(recipeId).map { checkResult(it) }
+    }
+    val recipe: LiveData<Recipe> = _recipe
+
+    val isDataAvailable: LiveData<Boolean> = _recipe.map { it.id != "NULL" }
+
+    // For refresh
+    private val _dataLoading = MutableLiveData<Boolean>()
+    val dataLoading: LiveData<Boolean> = _dataLoading
+
+    private val _editRecipeEvent = MutableLiveData<Event<Unit>>()
+    val editRecipeEvent: LiveData<Event<Unit>> = _editRecipeEvent
+
+    private val _deleteRecipeEvent = MutableLiveData<Event<Unit>>()
+    val deleteRecipeEvent: LiveData<Event<Unit>> = _deleteRecipeEvent
+
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarText: LiveData<Event<Int>> = _snackbarText
+
+    fun start(recipeId: String) {
+        if (_dataLoading.value == true || recipeId == _recipeId.value) {
+            return
+        }
+        _recipeId.value = recipeId
+    }
+
+    fun deleteRecipe() = viewModelScope.launch {
+        _recipeId.value?.let {
+            recipeRepository.deleteRecipe(it)
+            _deleteRecipeEvent.value = Event(Unit)
+        }
+    }
+
+    fun editRecipe() {
+        _editRecipeEvent.value = Event(Unit)
+    }
+
+    private fun checkResult(result: Result<Recipe>): Recipe {
+        return when (result) {
+            is Result.Success -> {
+                result.data
+            }
+            is Result.Loading -> {
+                Recipe(id = "NULL")
+            }
+            else -> {
+                showSnackbarMessage(R.string.error_loading_recipe)
+                Recipe(id = "NULL")
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(@StringRes message: Int) {
+        _snackbarText.value = Event(message)
+    }
 }
 
 @Suppress("UNCHECKED_CAST")
-class RecipeViewModelFactory (
+class RecipeDetailsViewModelFactory (
     private val recipeRepository: IRecipeRepository
 ): ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T =
