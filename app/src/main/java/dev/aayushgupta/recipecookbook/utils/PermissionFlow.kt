aa2a384c8e5c079bd00323277da2f1529c4ec3e4
Aayush Gupta <aayu.gupta.96@gmail.com>
2020-06-04 23:19:42 +0530

    add basic permission flow

---
+++ app/src/main/java/dev/aayushgupta/recipecookbook/utils/PermissionFlow.kt
@@ -0,0 +1,11 @@
+package dev.aayushgupta.recipecookbook.utils
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+
+object PermissionFlow {
+    fun triggerPermissionFlow(app: Application, ctx: Context, activity: Activity, onSuccess: () -> Unit, onFail: () -> Unit) {
+
+    }
+}
